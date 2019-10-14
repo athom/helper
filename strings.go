96a309fedc25aff63ec8d3886dc091502b05d38b
yeer <yeerkunth+github@gmail.com>
2019-10-14 22:07:34 +0800

    add pretty json

--- strings.go
+++ strings.go
@@ -1,7 +1,16 @@
 package helper
 
-import "strings"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
 
 func Oneline(multilineJson string) string {
 	return strings.Replace(multilineJson, "\n", "", -1)
+}
+
+func PrettyJson(str string) string {
+	json, err := json.MarshalIndent([]byte(str), "", "  ")
+	return fmt.Sprintf(json)
 }
