3e793afeddef6d000c884b867b4f275b1bbc5d2b
yeer <yeerkunth+github@gmail.com>
2019-10-14 22:17:20 +0800

    add ungly json func

---
+++ json.go
@@ -0,0 +1,33 @@
+package helper
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+func PrettyJson(str string) string {
+	var data map[string]interface{}
+	if err := json.Unmarshal([]byte(str), &data); err != nil {
+		panic(err)
+	}
+
+	json, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return "format error"
+	}
+	return fmt.Sprintf("%v", string(json))
+}
+
+func UglyJson(str string) string {
+	var data map[string]interface{}
+	if err := json.Unmarshal([]byte(str), &data); err != nil {
+		panic(err)
+	}
+
+	json, err := json.MarshalIndent(data, "", "")
+	if err != nil {
+		return "format error"
+	}
+	return fmt.Sprintf("%v", string(json))
+}
--- strings.go
+++ strings.go
@@ -1,24 +1,9 @@
 package helper
 
 import (
-	"encoding/json"
-	"fmt"
 	"strings"
 )
 
 func Oneline(multilineJson string) string {
 	return strings.Replace(multilineJson, "\n", "", -1)
-}
-
-func PrettyJson(str string) string {
-	var data map[string]interface{}
-	if err := json.Unmarshal([]byte(str), &data); err != nil {
-		panic(err)
-	}
-
-	json, err := json.MarshalIndent(data, "", "  ")
-	if err != nil {
-		return "format error"
-	}
-	return fmt.Sprintf("%v", string(json))
 }
