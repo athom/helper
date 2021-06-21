532678b3fd8c220f3b295b394163bbee3ed4c0d8
yeer <athom@126.com>
2021-06-22 00:35:55 +0800

    fix path

--- config.go
+++ config.go
@@ -11,7 +11,7 @@ func ReadYamlAsViperConfig(basename string) (r *viper.Viper) {
 	rootDir := GetEnv("ROOT_DIR", ".")
 	r = viper.New()
 	r.SetConfigType("yaml")
-	r.SetConfigName(basename)
+	r.SetConfigName("config")
 	r.AddConfigPath(fmt.Sprintf("%v/"+basename, rootDir))
 	err := r.ReadInConfig()
 	if err != nil {
