ac698c4d2456e07bb5c7a3ec8c7ed7eddfb603d5
yeer <athom@126.com>
2021-07-06 21:42:04 +0800

    add siteVerfications functions

--- config.go
+++ config.go
@@ -2,7 +2,9 @@ package helper
 
 import (
 	"fmt"
+	"html/template"
 	"log"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -19,4 +21,33 @@ func ReadYamlAsViperConfig(basename string) (r *viper.Viper) {
 		panic(err)
 	}
 	return r
+}
+
+type SiteVerificationItem struct {
+	Name    string `mapstructure:"name"`
+	Content string `mapstructure:"content"`
+}
+
+func GetSiteVerifications(config *viper.Viper, key string) (r []*SiteVerificationItem, err error) {
+	err = config.UnmarshalKey(key, &r)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func GetSiteVerificationsTemplate(config *viper.Viper, key string) (r template.HTML, err error) {
+	items, err := GetSiteVerifications(config, key)
+	if err != nil {
+		return
+	}
+
+	var lines []string
+	for _, sv := range items {
+		lines = append(lines, fmt.Sprintf(`<meta name="%v" content="%v" />`, sv.Name, sv.Content))
+	}
+	s := strings.Join(lines, " ")
+	r = template.HTML(s)
+	return
 }
